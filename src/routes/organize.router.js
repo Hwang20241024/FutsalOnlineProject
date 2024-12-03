944eb2971e251b56a53db225332b2af45a5e87b1
SmoothMonologue <mobugy99@gmail.com>
2024-12-03 20:55:45 +0900

    comment: 주석처리한 후자가 작동이 될지 의문

--- src/routes/organize.router.js
+++ src/routes/organize.router.js
@@ -8,7 +8,8 @@ router.post("/api/teams/cards", async (req, res, next) => {
   //슬릇 선택, 보유 중인 선수 카드 중 선택
   const { slotId, inventoryId } = req.body;
 
-  //const myteam= await team.findById(userId/*인증 어케하는지 공부한 뒤에 수정*/)
+  ///*인증 어케하는지 공부한 뒤에 수정*/
+  //const myteam= await prisma.team.findById(userId).exec();
 
   //선택된 선수
   const chosenMember = await prisma.inventory.findById(inventoryId).exec();
@@ -17,10 +18,31 @@ router.post("/api/teams/cards", async (req, res, next) => {
 
   //1? 2? 3?
   //선수 값이 유효하면 슬릇에 선수 삽입
-  if (chosenMember) {
-    chosenSlot.inventoryId = chosenMember.inventoryId;
+  switch (slotId) {
+    case inventoryId1:
+      if (chosenMember) {
+        chosenSlot.inventoryId1 = chosenMember.inventoryId;
+      }
+      await chosenSlot.save();
+    case inventoryId2:
+      if (chosenMember) {
+        chosenSlot.inventoryId2 = chosenMember.inventoryId;
+      }
+      await chosenSlot.save();
+    case inventoryId3:
+      if (chosenMember) {
+        chosenSlot.inventoryId3 = chosenMember.inventoryId;
+      }
+      await chosenSlot.save();
+    default:
   }
-  await chosenSlot.save();
+
+  //근데 이대로면 그냥
+  // if (chosenMember) {
+  //   chosenSlot.slotId = chosenMember.inventoryId;
+  // }
+  // await chosenSlot.save();
+  //굳이 스위치문 안 쓰고 이렇게 해도 되겠는데?
 
   return res.status(200).json({});
 });
