c3456ae1c1d3fac0bc595c3d0801ca57774ac181
SmoothMonologue <mobugy99@gmail.com>
2024-12-03 15:26:24 +0900

    refactor: url 변경

--- routes/organize.router.js
+++ routes/organize.router.js
@@ -3,6 +3,7 @@ import express from "express";
 const router = express.Router(); // express.Router()를 이용해 라우터를 생성합니다.
 
 //팀 편성 API
-router.put("/api/cards/set", async (req, res, next) => {
+router.post("/api/teams/cards", async (req, res, next) => {
   const { slot, inventoryId } = req.body;
 });
+
