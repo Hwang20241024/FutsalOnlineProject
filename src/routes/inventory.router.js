a4974d8b8bcd0fa965c9c129fe0ea9da6a7d9323
Hwang20241024 <ghkdaksdbs@naver.com>
2024-12-04 20:20:51 +0900

    feat: 보유 카드 상세조회API 생성

--- src/routes/inventory.router.js
+++ src/routes/inventory.router.js
@@ -44,4 +44,49 @@ router.get("/cards/inventory", authMiddleware, async (req, res, next) => {
   return res.status(200).json(inventorys);
 });
 
+
+/*보유 카드 상세*/
+router.get("/cards", authMiddleware, async (req, res, next) => {
+  const userId = req.user; // 유저 정보 가져오세요~
+  const {name} = req.body; // 바디 정보 가져오기.
+
+
+  // 인벤토리 조회
+  const inventorys = await prisma.inventory.findMany({
+    where:{
+      userId : userId,   // 유저 아이디로 필터링 
+      cards: {
+        name: {
+          equals: name,  // 카드 이름으로 필터링
+        },
+      },
+    },
+    select: {
+      inventoryId: true,
+      cardId: true,
+      upgrade: true,
+      cards: {
+        select:{
+          name:true,
+          speed:true,
+          pass:true,
+          sight:true,
+          tackle:true,
+          defence:true,
+          grade:true,
+        }
+      },
+    }
+  });
+
+  // 카드가 없다면
+  if(inventorys.length === 0) {
+    return next(new CustomError("검색하신 카드가 없습니다.", 404)); // 상태 코드 404: Not Found
+  }
+
+  // 이상없다면 출력
+  return res.status(200).json(inventorys);
+  
+})
+
 export default router;
