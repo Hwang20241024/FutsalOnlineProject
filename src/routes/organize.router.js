8cacc8b8d21b37cbacedaed42a1c8d6b33d71107
SmoothMonologue <mobugy99@gmail.com>
2024-12-04 14:01:31 +0900

    중복 편성 방지

--- src/routes/organize.router.js
+++ src/routes/organize.router.js
@@ -9,11 +9,11 @@ router.post("/api/teams/cards", async (req, res, next) => {
   const { userId } = req.user;
   //슬릇 선택, 보유 중인 선수 카드 중 선택
   const { slotId, inventoryId } = req.body;
-  
-  // //선택된 선수
-  // const chosenMember = await prisma.inventory.findById(inventoryId).exec();
-  // //선택된 슬릇
-  // const chosenSlot = await prisma.team.findOne(slotId).exec();
+
+  //선택된 선수
+  const chosenMember = await prisma.inventory.findById(inventoryId).exec();
+  //선택된 슬릇
+  const chosenSlot = await prisma.team.findOne(slotId).exec();
 
   // 연결
   const user = await prisma.users.findFirst({
@@ -24,15 +24,19 @@ router.post("/api/teams/cards", async (req, res, next) => {
 
   await prisma.$transaction(async (prisma) => {
     //중복 편성 방지
-    // for (let i = 1; i < 4; i++) {
-    //   let check = "inventoryId" + i;
-    //조건 재설정해야함
-    //   if (chosenSlot.check == chosenMember.inventoryId) {
-    //     return res
-    //       .status(405)
-    //       .json({ message: `이미 투입되어있는 선수입니다.` });
-    //   }
-    // }
+    for (let i = 1; i < 4; i++) {
+      let check = "inventoryId" + i;
+      //조건 맞는지 확인해야함
+      if (
+        chosenSlot.inventoryId1 == chosenMember.inventoryId ||
+        chosenSlot.inventoryId2 == chosenMember.inventoryId ||
+        chosenSlot.inventoryId3 == chosenMember.inventoryId
+      ) {
+        return res
+          .status(405)
+          .json({ message: `이미 투입되어있는 선수입니다.` });
+      }
+    }
 
     //선택한 선수 포지션 결정
     await prisma.team.update({
@@ -44,7 +48,7 @@ router.post("/api/teams/cards", async (req, res, next) => {
       },
     });
 
-    //만약 slotId로 못 받을 경우 위에 거 말고 스위치 문으로 시도
+    //만약 slotId로 못 받는 경우 위에 거 말고 스위치 문으로 시도
     // switch (slotId) {
     //   case inventoryId1:
     //     await prisma.team.update({
