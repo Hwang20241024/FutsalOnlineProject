9cde5d69c2ac8438e10b2bc43700c55145f2f9ff
Hwang20241024 <ghkdaksdbs@naver.com>
2024-12-04 16:35:16 +0900

    feat: 내정보 보기 API 생성

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -82,4 +82,50 @@ router.post("/sign-in", async (req, res, next) => {
   return res.status(200).json({ message: "로그인 성공" });
 });
 
+/** 풋살온라인 - 내정보 보기 API → (JWT 인증 필요)  **/
+router.get("/users/me", authMiddleware, async (req, res, next) => {
+  const userId = req.user; // 유저 정보 가져오세요~
+
+  // 1. 유저 정보를 가져온다.
+  const users = await prisma.users.findMany({
+    select: {
+      userId: true, // userId 선택
+      id: true, // id 컬럼만 선택
+      mmr: true, // mmr 컬럼만 선택
+      cash: true,
+    },
+    orderBy: [
+      {
+        mmr: "desc", // mmr을 내림차순으로 정렬
+      },
+      {
+        userId: "asc", // 만약에 mmr이 똑같으면 가입 순서대로.
+      },
+    ],
+  });
+
+  // 2. 유저가 없을때 예외 설정.
+  if (users.length === 0) {
+    return next(new CustomError("유저가 없습니다.", 404)); // 상태 코드 404: Not Found
+  }
+
+  // 3. 순위를 매긴다. 
+  const rankedUsers = users.map((user, index) => ({
+    ranking: index + 1, // 인덱스가 0부터 시작, + 1을 해야한다.
+    ...user,
+  }));
+
+  // 순위를 정하고 내정보를 가져오자.
+  const myUser = rankedUsers.find(user => user.userId === userId);
+
+  // 출력
+  return res.status(201).json({
+    ranking: myUser.ranking,
+    id: myUser.id,
+    mmr: myUser.mmr,
+    cash: myUser.cash,
+  });
+
+})
+
 export default router;
